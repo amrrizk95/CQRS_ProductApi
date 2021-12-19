cd977c1c5b552637119225389933a5d0a2f1eb45
amr rizk <amr.rizk@inthekloud.com>
2021-12-19 23:50:54 +0200

    Add project files.

---
+++ CQRS.WebApi/Context/IApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS.WebApi.Context
+{
+  public  interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ CQRS.WebApi/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,39 @@
+﻿using CQRS.WebApi.Context;
+using CQRS.WebApi.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.WebApi.Queries
+{
+    public class GetAllProductsQuery:IRequest<IEnumerable<Product>>
+    {
+
+        public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<Product>>
+        {
+            private readonly IApplicationContext _context;
+            public GetAllProductsQueryHandler(IApplicationContext context)
+            {
+                _context = context;
+            }
+            public async Task<IEnumerable<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+            {
+                var productList = await _context.Products.ToListAsync();
+                if (productList == null)
+                {
+                    return null;
+                }
+                return productList.AsReadOnly();
+            }
+        }
+
+
+
+       
+
+    }
+}
